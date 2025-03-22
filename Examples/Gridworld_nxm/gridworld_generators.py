8b1cff32ac0e7e33b01d8cb1cfb6c5d1287034d7
turtlesoul25 <77328554+turtlesoul25@users.noreply.github.com>
2025-03-22 15:40:49 +0000

    Tested basic implementation of gridworld; updated reward generator

--- Examples/Gridworld_nxm/gridworld_generators.py
+++ Examples/Gridworld_nxm/gridworld_generators.py
@@ -6,7 +6,7 @@ def gen_next_pos(s, a, S):
     return moves[a] if moves[a] in S else s # return new position unless crossing boundary
 
 
-def reward_func_generator(reward_states, boundary_penalty S):
+def reward_func_generator(reward_states, boundary_penalty, S):
     '''
     Defines a Reward function (input for value iteration) given a 
     dictionary of rewards for chosen states s from S (set of states)
@@ -30,7 +30,7 @@ def prob_func_generator(intended_move_prob, S, A):
         unintended_A.remove(a)
         if sp == gen_next_pos(s, a, S):
             return intended_move_prob
-        elif sp in [gen_next_pos(s, a_alt, Sg) for a_alt in unintended_A]:
+        elif sp in [gen_next_pos(s, a_alt, S) for a_alt in unintended_A]:
             return (1-intended_move_prob)/3
         else:
             return 0
