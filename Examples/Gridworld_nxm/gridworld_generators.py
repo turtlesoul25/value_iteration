208931698eea7537f24395f466709d6448eb35d0
turtlesoul25 <77328554+turtlesoul25@users.noreply.github.com>
2025-03-22 17:00:28 +0000

    Documentation updated for gridworld_generators.py; updated writeup and results for gridworld_example notebook

--- Examples/Gridworld_nxm/gridworld_generators.py
+++ Examples/Gridworld_nxm/gridworld_generators.py
@@ -1,4 +1,6 @@
-def gen_next_pos(s, a, S):
+from typing import Set, Callable, Dict
+
+def gen_next_pos(s, a, S: Set):
     '''Generates the next state if action a is implemented at state s'''
     i, j = s
     moves = {"UP": (i-1, j), "DOWN": (i+1, j), 
@@ -6,19 +8,21 @@ def gen_next_pos(s, a, S):
     return moves[a] if moves[a] in S else s # return new position unless crossing boundary
 
 
-def reward_func_generator(reward_states, boundary_penalty, S):
+def reward_func_generator(reward_states: Dict, boundary_penalty, S: Set) -> Callable:
     '''
     Defines a Reward function (input for value iteration) given a 
     dictionary of rewards for chosen states s from S (set of states)
     '''
     def R(s, a):
-        s_next = gen_next_pos(s, a, S)
-        if s_next == s:
-            return -boundary_penalty # penalty if we cross the grid boundary
+        '''Function to calculate reward when action a is performed at state s.'''
+        s_next = gen_next_pos(s, a, S) # find new state
+        if s_next == s: # penalty if we cross the grid boundary
+            return -boundary_penalty 
+        # default reward is 0 unless otherwise specified
         return reward_states[s_next] if s_next in reward_states else 0
     return R
 
-def prob_func_generator(intended_move_prob, S, A):
+def prob_func_generator(intended_move_prob, S: Set, A: Set) -> Callable:
     '''
     Defines a transition probability function (input for value iteration)
     given the deterministic/stochastic probability of moving in the intended
@@ -26,14 +30,16 @@ def prob_func_generator(intended_move_prob, S, A):
     (if there are 4 actions U, L, D, R).
     '''
     def P(sp, s, a):
-        unintended_A = A.copy()
-        unintended_A.remove(a)
+        '''Function to determine transition probability of moving to 
+        state s' given we perform action a at state s.'''
+        unintended_A = A.copy() # set of unintended actions
+        unintended_A.remove(a) # remove intended action
         if sp == gen_next_pos(s, a, S):
-            return intended_move_prob
+            return intended_move_prob # if we go in the intended direction
         elif sp in [gen_next_pos(s, a_alt, S) for a_alt in unintended_A]:
-            return (1-intended_move_prob)/3
+            return (1-intended_move_prob)/3 # equal probability for each unintended direction
         else:
-            return 0
+            return 0 # landing in other states that cannot be reached in one move
         
     return P
 
